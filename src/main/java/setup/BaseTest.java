e975fde47bfb0e460bdd71532faabb291aad2064
Mari Zueva <mzueva403@gmail.com>
2021-06-25 09:18:11 +0300

    fix appPackage for android native test

--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -3,7 +3,10 @@ package setup;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pageObjects.PageObject;
 
 import java.io.File;
@@ -29,18 +32,17 @@ public class BaseTest implements IDriver {
     }
 
     @Parameters({"platformName","appType","deviceName", "udid",
-            "browserName","app", "appPackage", "appActivity", "bundleId"})
+            "browserName","appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
     public void setUp(String platformName, String appType,
                       @Optional("") String deviceName,
                       @Optional("") String udid,
                       @Optional("") String browserName,
-                      @Optional("") String app,
                       @Optional("") String appPackage,
                       @Optional("") String appActivity,
                       @Optional("") String bundleId) throws Exception {
         System.out.println("Before: app type - "+appType);
-        setAppiumDriver(platformName, deviceName, udid, browserName, app, appPackage, appActivity, bundleId);
+        setAppiumDriver(platformName, deviceName, udid, browserName, appPackage, appActivity, bundleId);
         setPageObject(appType, appiumDriver);
         driverWait = new WebDriverWait(appiumDriver, 10);
     }
@@ -52,15 +54,13 @@ public class BaseTest implements IDriver {
     }
 
     private void setAppiumDriver(String platformName, String deviceName, String udid,
-                                 String browserName, String app, String appPackage,
+                                 String browserName, String appPackage,
                                  String appActivity, String bundleId){
         DesiredCapabilities capabilities = new DesiredCapabilities();
         //mandatory Android capabilities
         capabilities.setCapability("platformName", platformName);
         capabilities.setCapability("deviceName", deviceName);
         capabilities.setCapability("udid", udid);
-
-        if(app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
 
         capabilities.setCapability("browserName", browserName);
         capabilities.setCapability("chromedriverDisableBuildCheck","true");
@@ -78,7 +78,7 @@ public class BaseTest implements IDriver {
 
         // Timeouts tuning
         appiumDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        appiumDriver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+
     }
 
     private void setPageObject(String appType, AppiumDriver appiumDriver) throws Exception {
